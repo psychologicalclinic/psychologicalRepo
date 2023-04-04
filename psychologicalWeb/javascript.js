ca4848ec14ed809c35168ddcd4a3b1373dde6876
seymabltci <123351461+seymabltci@users.noreply.github.com>
2023-04-04 10:21:53 +0300

    Add files via upload

---
+++ psychologicalWeb/javascript.js
@@ -0,0 +1,32 @@
+
+
+
+
+function hakkımda() {
+  var x = document.getElementById("page2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function hizmetlerim() {
+    var x = document.getElementById("portfolio");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "block";
+    }
+  }
+ 
+  
+
+function changepage() {
+    var x = document.getElementById("page2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
